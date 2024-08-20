bc306b33afd8c4399579892536c4f7f7579772eb
Joseph Nyambura <joenyambura@gmail.com>
2024-08-20 13:38:49 +0300

    handle array scripts correctly
    Updated TrackingCodeTags.php to handle array scripts correctly

--- src/TrackingCodeTags.php
+++ src/TrackingCodeTags.php
@@ -23,35 +23,39 @@ class TrackingCodeTags extends Tags
     public function createLink(string $tag): string
     {
         $values = collect(YAML::file(__DIR__.'/../content/tracking-codes.yaml')->parse())
-        ->merge(YAML::file(base_path('content/tracking-codes.yaml'))->parse())
-        ->all();
+            ->merge(YAML::file(base_path('content/tracking-codes.yaml'))->parse())
+            ->all();
 
-        if(!isset($values['enabled']) || $values['enabled'] == false || !isset($values['scripts'])){
+        if (!isset($values['enabled']) || $values['enabled'] == false || !isset($values['scripts'])) {
             return '';
         }
         
-        $filtered = Arr::where($values['scripts'], function ($value, $key) use($tag) {
+        $filtered = Arr::where($values['scripts'], function ($value, $key) use ($tag) {
             return $value['position'] == $tag && $value['enabled'] == true;
         });
 
         $scripts = Arr::pluck($filtered, 'script');
 
         $output = '<!-- tracking code manager '.$tag.' scripts -->';
-        foreach($scripts as $script){
-            $output .= $script;
+
+        foreach ($scripts as $script) {
+            if (is_string($script)) {
+                $output .= $script;
+            } elseif (is_array($script) && isset($script['code'])) {
+                // Extract and append the actual code from the array
+                $output .= $script['code'];
+            } else {
+                $output .= '<!-- Non-string script detected -->';
+                $output .= print_r($script, true); // For debugging purposes
+            }
         }
         $output .= '<!-- end tracking code manager '.$tag.' scripts -->';
+        
         return $output;
-
-        return '';
     }
 
     protected function isSupportedChannel(string $channel): bool
     {
-        if (in_array($channel, $this->channels)) {
-            return true;
-        }
-        return false;
+        return in_array($channel, $this->channels);
     }
-
 }
